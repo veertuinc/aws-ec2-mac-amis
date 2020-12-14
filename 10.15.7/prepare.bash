77b7fcd38a179fe6081d81b5bd01b39d519eb74e
Nathan Pierce <connarpierce@gmail.com>
2020-12-14 12:09:45 -0500

    quick fix

--- 10.15.7/prepare.bash
+++ 10.15.7/prepare.bash
@@ -5,6 +5,8 @@ cd $SCRIPT_DIR
 . ../_helpers.bash
 # Perform changes that all macOS versions support + install anka virtualization
 . ../_common-prep.bash
-# Create plist for cloud connect
-[[ ! -e $CLOUD_CONNECT_PLIST_PATH ]] && sudo -E bash -c "../cloud-connect.bash"
 [[ ! -e $RESIZE_DISK_PLIST_PATH ]] && sudo -E bash -c "../resize-disk.bash"
+
+
+# Create plist for cloud connect # Should be last!
+[[ ! -e $CLOUD_CONNECT_PLIST_PATH ]] && sudo -E bash -c "../cloud-connect.bash"
