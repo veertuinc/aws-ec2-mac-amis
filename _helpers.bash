781b3aaa27e87be1a22920855d9baead3f5be4bb
Nathan Pierce <connarpierce@gmail.com>
2020-12-14 13:17:51 -0500

    beta

--- _helpers.bash
+++ _helpers.bash
@@ -2,9 +2,6 @@ LAUNCH_LOCATION="/Library/LaunchDaemons/"
 CLOUD_CONNECT_PLIST_PATH="${LAUNCH_LOCATION}com.veertu.aws-ec2-mac-amis.cloud-connect.plist"
 RESIZE_DISK_PLIST_PATH="${LAUNCH_LOCATION}com.veertu.aws-ec2-mac-amis.resize-disk.plist"
 
-# Check if user-data exists
-[[ ! -z "$(curl -s http://169.254.169.254/latest/user-data | grep 404)" ]] && echo "Could not find required ANKA_CONTROLLER_ADDRESS in instance user-data!" && exit 1
-
 modify_hosts() {
   [[ -z $1 ]] && echo "ARG 1 missing" && exit 1
   [[ -z $1 ]] && echo "ARG 2 missing" && exit 1
