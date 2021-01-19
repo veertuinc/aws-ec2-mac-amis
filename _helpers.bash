4723061e1259fc337e19423d29982f82d240d1d2
Nathan Pierce <connarpierce@gmail.com>
2021-01-19 11:32:22 -0500

    PATH fix

--- _helpers.bash
+++ _helpers.bash
@@ -1,6 +1,7 @@
 LAUNCH_LOCATION="/Library/LaunchDaemons/"
 CLOUD_CONNECT_PLIST_PATH="${LAUNCH_LOCATION}com.veertu.aws-ec2-mac-amis.cloud-connect.plist"
 RESIZE_DISK_PLIST_PATH="${LAUNCH_LOCATION}com.veertu.aws-ec2-mac-amis.resize-disk.plist"
+PATH=/usr/local/bin:$PATH # /Users/ec2-user/aws-ec2-mac-amis/cloud-connect.bash: line 17: jq: command not found
 
 modify_hosts() {
   [[ -z $1 ]] && echo "ARG 1 missing" && exit 1
