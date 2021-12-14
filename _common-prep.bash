a6909a45ec79ab7a25a3c154a149ba542966e0a1
Nathan Pierce <connarpierce@gmail.com>
2021-12-14 16:59:17 -0600

    create a vm quickly to create folders

--- _common-prep.bash
+++ _common-prep.bash
@@ -41,3 +41,7 @@ sudo pmset -a hibernatemode 0
 # Optimizations for templates
 anka config chunk_size 2147483648
 sudo anka config chunk_size 2147483648
+
+# Required to create necessary folders | No such file or directory: '/var/root/Library/Application Support/Veertu/Anka/img_lib/' from agent
+anka create test && anka delete --yes test
+sudo anka create test && sudo anka delete --yes test
