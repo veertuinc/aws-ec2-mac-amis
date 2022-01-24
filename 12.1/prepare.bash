849f63040ad6b16ed6648483497cdb05bf2f4fb8
Nathan Pierce <connarpierce@gmail.com>
2022-01-24 11:06:22 -0500

    quick fix

--- 12.1/prepare.bash
+++ 12.1/prepare.bash
@@ -10,3 +10,5 @@ cd $SCRIPT_DIR
 [[ ! -e $CLOUD_CONNECT_PLIST_PATH ]] && sudo -E bash -c "../cloud-connect.bash"
 sudo chown -R $AWS_INSTANCE_USER:staff ~/aws-ec2-mac-amis
 # sudo anka config vmx_mitigations 0 # Apple has locked this and it can no longer be modified
+echo ""
+echo "]] SUCCESS"
