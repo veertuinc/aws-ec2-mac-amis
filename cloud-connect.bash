918415031879ceaf0d547d610c44999922a4ef8a
Nathan Pierce <norsegaud@gmail.com>
2022-04-20 18:37:27 -0400

    better user-data message and wrapper for disjoin

--- cloud-connect.bash
+++ cloud-connect.bash
@@ -55,7 +55,7 @@ else
   echo "$(date) ($(whoami)): Attempting join..."
   # Check if user-data exists
   if [[ ! -z "$(curl -s http://169.254.169.254/latest/user-data | grep 404)" || -z "$(curl -s http://169.254.169.254/latest/user-data)" ]]; then
-    echo "Could not find required ANKA_CONTROLLER_ADDRESS in instance user-data!"
+    echo "Could not find any user-data for instance..."
     disjoin || true
     exit 1
   fi
