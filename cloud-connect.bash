7fb10e452c98c8491f28faf990650c18a1c0f9aa
Nathan Pierce <norsegaud@gmail.com>
2022-05-06 12:34:10 -0400

    ANKA-2901: support for controller and registry certs for API calls

--- cloud-connect.bash
+++ cloud-connect.bash
@@ -12,12 +12,8 @@ disjoin() {
   rm -f /tmp/wait-fifo
   /usr/local/bin/ankacluster disjoin &
   if [[ -n "${ANKA_CONTROLLER_ADDRESS}" ]]; then
-    CERTS=""
-    [[ ! -z "$CLOUD_CONNECT_CERT" ]] && CERTS="--cert $CLOUD_CONNECT_CERT"
-    [[ ! -z "$CLOUD_CONNECT_KEY" ]] && CERTS="$CERTS --cert-key $CLOUD_CONNECT_KEY"
-    [[ ! -z "$CLOUD_CONNECT_CA" ]] && CERTS="$CERTS --cacert $CLOUD_CONNECT_CA"
-    NODE_ID="$(curl -s $CERTS "${ANKA_CONTROLLER_ADDRESS}/api/v1/node" | jq -r ".body | .[] | select(.node_name==\"$(hostname)\") | .node_id")"
-    curl -s $CERTS -X DELETE "${ANKA_CONTROLLER_ADDRESS}/api/v1/node" -H "Content-Type: application/json" -d "{\"node_id\": \"$NODE_ID\"}"
+    NODE_ID="$(curl -s ${ANKA_CONTROLLER_API_CERTS} "${ANKA_CONTROLLER_ADDRESS}/api/v1/node" | jq -r ".body | .[] | select(.node_name==\"$(hostname)\") | .node_id")"
+    curl -s ${ANKA_CONTROLLER_API_CERTS} -X DELETE "${ANKA_CONTROLLER_ADDRESS}/api/v1/node" -H "Content-Type: application/json" -d "{\"node_id\": \"$NODE_ID\"}"
   fi
   wait $!
 }
@@ -66,8 +62,29 @@ else
   INSTANCE_PRIVATE_IP="$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)"
   INSTANCE_PUBLIC_IP="$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)"
   # IF the user wants to change the IP address for the registry domain name (if they want to use a second EC2 registry for better speed), handle setting the /etc/hosts
-  if [[ ! -z "$ANKA_REGISTRY_OVERRIDE_IP" && ! -z "$ANKA_REGISTRY_OVERRIDE_DOMAIN" ]]; then
+  if [[ -n "${ANKA_REGISTRY_OVERRIDE_IP}" && -n "${ANKA_REGISTRY_OVERRIDE_DOMAIN}" ]]; then
     modify_hosts $ANKA_REGISTRY_OVERRIDE_DOMAIN $ANKA_REGISTRY_OVERRIDE_IP
+  fi
+  # Certificate support
+  ANKA_CONTROLLER_API_CERTS="${ANKA_CONTROLLER_API_CERTS:-""}"
+  if [[ -z "${ANKA_CONTROLLER_API_CERTS}" && -n "${ANKA_CONTROLLER_API_CERT}" ]]; then
+    ANKA_CONTROLLER_API_CERTS="--cert ${ANKA_CONTROLLER_API_CERT}"
+    if [[ -n "${ANKA_CONTROLLER_API_KEY}" ]]; then
+      ANKA_CONTROLLER_API_CERTS="${ANKA_CONTROLLER_API_CERTS} --cert-key ${ANKA_CONTROLLER_API_KEY}"
+    else
+      echo "missing controller cert key" && exit 2
+    fi
+    [[ -n "${ANKA_CONTROLLER_API_CA}" ]] && ANKA_CONTROLLER_API_CERTS="${ANKA_CONTROLLER_API_CERTS} --cacert ${ANKA_CONTROLLER_API_CA}"
+  fi
+  ANKA_REGISTRY_API_CERTS="${ANKA_REGISTRY_API_CERTS:-""}"
+  if [[ -z "${ANKA_REGISTRY_API_CERTS}" && -n "${ANKA_REGISTRY_API_CERT}" ]]; then
+    ANKA_REGISTRY_API_CERTS="--cert ${ANKA_REGISTRY_API_CERT}"
+    if [[ -n "${ANKA_REGISTRY_API_KEY}" ]]; then
+      ANKA_REGISTRY_API_CERTS="${ANKA_REGISTRY_API_CERTS} --cert-key ${ANKA_REGISTRY_API_KEY}"
+    else
+      echo "missing registry cert key" && exit 2
+    fi
+    [[ -n "${ANKA_REGISTRY_API_CA}" ]] && ANKA_REGISTRY_API_CERTS="${ANKA_REGISTRY_API_CERTS} --cacert ${ANKA_REGISTRY_API_CA}"
   fi
   # Join arguments
   ANKA_JOIN_ARGS="${ANKA_JOIN_ARGS:-"$*"}" # used for older getting started script + enables overriding defaults from inside plist instead of user-data
@@ -76,7 +93,7 @@ else
   ${ANKA_USE_PUBLIC_IP:-false} && INSTANCE_IP="${INSTANCE_PUBLIC_IP}" || INSTANCE_IP="${INSTANCE_PRIVATE_IP}"
   [[ ! "${ANKA_JOIN_ARGS}" =~ "--host" ]] && ANKA_JOIN_ARGS="${ANKA_JOIN_ARGS} --host ${INSTANCE_IP}"
   # Anka agent install to handle it failing
-  curl -O ${ANKA_CONTROLLER_ADDRESS}/pkg/AnkaAgent.pkg && installer -pkg AnkaAgent.pkg -tgt / && rm -f AnkaAgent.pkg
+  curl ${ANKA_CONTROLLER_API_CERTS} -O "${ANKA_CONTROLLER_ADDRESS}/pkg/AnkaAgent.pkg" && installer -pkg AnkaAgent.pkg -tgt / && rm -f AnkaAgent.pkg
   anka license accept-eula 2>/dev/null || true
   if [[ -n "${ANKA_LICENSE}" ]]; then # Activate license if present
     anka license show
@@ -85,14 +102,14 @@ else
       ANKA_LICENSE_ACTIVATE_STDOUT="$(anka license activate -f "${ANKA_LICENSE}" || true)"
       echo "${ANKA_LICENSE_ACTIVATE_STDOUT}"
       # Post the fulfillment ID to the centralized logs
-      ANKA_CONTROLLER_CONFIG_REGISTRY_ADDRESS="$(curl -s ${ANKA_CONTROLLER_ADDRESS}/api/v1/status | jq -r '.body.registry_address')"
-      curl -v "${ANKA_CONTROLLER_CONFIG_REGISTRY_ADDRESS}/log" -d "{\"machine_name\": \"${INSTANCE_ID} | $(system_profiler SPHardwareDataType | grep 'Hardware UUID' | awk '{print $NF}')\", \"service\": \"AWS Cloud Connect Service\", \"host\": \"\", \"content\": \"${ANKA_LICENSE_ACTIVATE_STDOUT}\"}"
+      ANKA_CONTROLLER_CONFIG_REGISTRY_ADDRESS="$(curl -s ${ANKA_CONTROLLER_API_CERTS} ${ANKA_CONTROLLER_ADDRESS}/api/v1/status | jq -r '.body.registry_address')"
+      curl ${ANKA_REGISTRY_API_CERTS} -v "${ANKA_CONTROLLER_CONFIG_REGISTRY_ADDRESS}/log" -d "{\"machine_name\": \"${INSTANCE_ID} | $(system_profiler SPHardwareDataType | grep 'Hardware UUID' | awk '{print $NF}')\", \"service\": \"AWS Cloud Connect Service\", \"host\": \"\", \"content\": \"${ANKA_LICENSE_ACTIVATE_STDOUT}\"}"
     fi
     anka license show
   fi
   /usr/local/bin/ankacluster disjoin || true
   sleep 10 # AWS instances, on first start, and even with functional networking (we ping github.com above), will have 169.254.169.254 assigned to the default interface and since joining happens very early in the startup process, that'll be what is assigned in the controller and cause problems.
-  /usr/local/bin/ankacluster join $ANKA_CONTROLLER_ADDRESS $ANKA_JOIN_ARGS
+  /usr/local/bin/ankacluster join ${ANKA_CONTROLLER_ADDRESS} ${ANKA_JOIN_ARGS}
   trap disjoin 0 # Disjoin after we joined properly to avoid unloading prematurely
   set +x
   echo "Joined and now we'll stay alive and wait for a shutdown signal..."
