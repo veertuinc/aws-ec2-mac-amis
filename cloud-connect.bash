c97b5dfcfcab94a1e770d62bcd71365489c28f7f
Nathan Pierce <norsegaud@gmail.com>
2022-05-06 11:32:43 -0400

    bug fix: http:// in urls being curled

--- cloud-connect.bash
+++ cloud-connect.bash
@@ -85,8 +85,8 @@ else
       ANKA_LICENSE_ACTIVATE_STDOUT="$(anka license activate -f "${ANKA_LICENSE}" || true)"
       echo "${ANKA_LICENSE_ACTIVATE_STDOUT}"
       # Post the fulfillment ID to the centralized logs
-      ANKA_CONTROLLER_CONFIG_REGISTRY_ADDRESS="$(curl -s http://${ANKA_CONTROLLER_ADDRESS}/api/v1/status | jq -r '.body.registry_address')"
-      curl -v "http://${ANKA_CONTROLLER_CONFIG_REGISTRY_ADDRESS}/log" -d "{\"machine_name\": \"${INSTANCE_ID} | $(system_profiler SPHardwareDataType | grep 'Hardware UUID' | awk '{print $NF}')\", \"service\": \"AWS Cloud Connect Service\", \"host\": \"\", \"content\": \"${ANKA_LICENSE_ACTIVATE_STDOUT}\"}"
+      ANKA_CONTROLLER_CONFIG_REGISTRY_ADDRESS="$(curl -s ${ANKA_CONTROLLER_ADDRESS}/api/v1/status | jq -r '.body.registry_address')"
+      curl -v "${ANKA_CONTROLLER_CONFIG_REGISTRY_ADDRESS}/log" -d "{\"machine_name\": \"${INSTANCE_ID} | $(system_profiler SPHardwareDataType | grep 'Hardware UUID' | awk '{print $NF}')\", \"service\": \"AWS Cloud Connect Service\", \"host\": \"\", \"content\": \"${ANKA_LICENSE_ACTIVATE_STDOUT}\"}"
     fi
     anka license show
   fi
