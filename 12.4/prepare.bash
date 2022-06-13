494797edb7a1c9fce8c94eb78ca101acbe4f5f5c
Nathan Pierce <norsegaud@gmail.com>
2022-06-13 09:20:33 -0400

    12.4

---
+++ 12.4/prepare.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -exo pipefail
+unset HISTFILE
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $SCRIPT_DIR
+. ../_helpers.bash
+# Perform changes that all macOS versions support + install anka virtualization
+. ../_common-prep.bash
+[[ ! -e $RESIZE_DISK_PLIST_PATH ]] && sudo -E bash -c "pwd; ../resize-disk.bash"
+# Create plist for cloud connect # Should be last!
+[[ ! -e $CLOUD_CONNECT_PLIST_PATH ]] && sudo -E bash -c "../cloud-connect.bash"
+sudo chown -R $AWS_INSTANCE_USER:staff ~/aws-ec2-mac-amis
+# sudo anka config vmx_mitigations 0 # Apple has locked this and it can no longer be modified
+[[ -z "$(sudo sysadminctl -secureTokenStatus ec2-user 2>&1 | grep ENABLED)" ]] && sudo /usr/bin/dscl . -passwd /Users/ec2-user "${AWS_INSTANCE_USER_PASSWD}"
+echo ""
+echo "]] SUCCESS"
+unset HISTFILE
