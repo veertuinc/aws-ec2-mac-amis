cfbcd7dc8533ffed7e965f70a132eb90f170747a
Nathan Pierce <norsegaud@gmail.com>
2022-11-16 13:37:56 -0500

    new arm brew location for PATH

--- cloud-connect.bash
+++ cloud-connect.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -exo pipefail
 [[ ! $EUID -eq 0 ]] && echo "RUN AS ROOT!" && exit 1
+export PATH="${PATH}:$(brew --prefix)/bin:$(brew --prefix)/sbin" # support new arm brew location
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $SCRIPT_DIR
 echo "Waiting for networking..."
