f8e59dd91ef99ec4eb8b9dec555d27f1436aeca2
Nathan <NorseGaud@users.noreply.github.com>
2024-10-23 07:54:39 -0500

    Update anklet-install.bash

--- scripts/anklet-install.bash
+++ scripts/anklet-install.bash
@@ -37,8 +37,8 @@ ARCH=$([[ $(arch) == "arm64" ]] && echo "arm64" || echo "amd64")
 LATEST_VERSION=$(curl -sL https://api.github.com/repos/veertuinc/anklet/releases | jq -r '.[0].tag_name')
 curl -L -O https://github.com/veertuinc/anklet/releases/download/${LATEST_VERSION}/anklet_v${LATEST_VERSION}_darwin_${ARCH}.zip
 unzip anklet_${LATEST_VERSION}_darwin_${ARCH}.zip
-chmod +x anklet_${LATEST_VERSION}_darwin_${ARCH}
-cp anklet_${LATEST_VERSION}_darwin_${ARCH} /usr/local/bin/anklet
+chmod +x anklet_v${LATEST_VERSION}_darwin_${ARCH}
+cp anklet_v${LATEST_VERSION}_darwin_${ARCH} /usr/local/bin/anklet
 mkdir -p ~/.config/
 sudo chown -R $AWS_INSTANCE_USER:staff ~/.config
 cd ~/.config/
