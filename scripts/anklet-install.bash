8c14ed9af9e91c9042426cc2a971322a04f1d1af
Nathan Pierce <norsegaud@gmail.com>
2024-11-12 11:34:09 -0600

    quick fix

--- scripts/anklet-install.bash
+++ scripts/anklet-install.bash
@@ -2,7 +2,7 @@
 set -exo pipefail
 unset HISTFILE
 if [ "$EUID" -ne 0 ]; then
-  echo "Please run as root using sudo create-plist.bash"
+  echo "Please run as root using sudo anklet-install.bash"
   exit 1
 fi
 . ./_helpers.bash
@@ -36,20 +36,24 @@ EOF
 ARCH=$([[ $(arch) == "arm64" ]] && echo "arm64" || echo "amd64")
 LATEST_VERSION=$(curl -sL https://api.github.com/repos/veertuinc/anklet/releases | jq -r '.[0].tag_name')
 curl -L -O https://github.com/veertuinc/anklet/releases/download/"${LATEST_VERSION}"/anklet_v"${LATEST_VERSION}"_darwin_"${ARCH}".zip
-unzip anklet_v"${LATEST_VERSION}"_darwin_"${ARCH}".zip
+unzip -f anklet_v"${LATEST_VERSION}"_darwin_"${ARCH}".zip
 chmod +x anklet_v"${LATEST_VERSION}"_darwin_"${ARCH}"
 cp anklet_v"${LATEST_VERSION}"_darwin_"${ARCH}" /usr/local/bin/anklet
 mkdir -p ~/.config/
-sudo chown -R $AWS_INSTANCE_USER:staff ~/.config
+[[ $(whoami) == "ec2-user" ]] && sudo chown -R $AWS_INSTANCE_USER:staff ~/.config
 cd ~/.config/
-git clone --no-checkout --depth=1 --filter=blob:none https://github.com/veertuinc/anklet.git
-pushd anklet
-  git reset -q -- \
-    plugins
-  git checkout-index -a -f
-popd
+if [[ ! -d anklet ]]; then
+  git clone --no-checkout --depth=1 --filter=blob:none https://github.com/veertuinc/anklet.git
+  pushd anklet
+    git reset -q -- \
+      plugins
+    git checkout-index -a -f
+  popd
+fi
 if [[ -n "${ANKA_EXECUTE_SCRIPT_CONFIG}" ]]; then
   echo "${ANKA_EXECUTE_SCRIPT_CONFIG}" > ~/.config/anklet/config.yml
+else 
+  echo "WARNING: Be sure to create your ~/.config/anklet/config.yml before loading!"
 fi
 echo "Anklet has been installed and loaded."
 echo "You can control it with the following commands:"
@@ -57,4 +61,3 @@ echo "  sudo launchctl start com.veertu.anklet"
 echo "  sudo launchctl stop com.veertu.anklet"
 echo "  sudo launchctl unload -w /Library/LaunchDaemons/com.veertu.anklet.plist"
 echo "  sudo launchctl load -w /Library/LaunchDaemons/com.veertu.anklet.plist"
-echo "Be sure to create your ~/.config/anklet/config.yml before loading!"
