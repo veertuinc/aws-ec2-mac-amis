4e91be24e484bfdb6310fc0c23c2baa8b5229ced
Nathan Pierce <norsegaud@gmail.com>
2025-11-26 09:38:47 -0600

    prepare-eternal for new m4s

---
+++ prepare-external-disk.bash
@@ -0,0 +1,65 @@
+#!/bin/bash
+set -exo pipefail
+[[ ! $EUID -eq 0 ]] && echo "RUN AS ROOT!" && exit 1
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $SCRIPT_DIR
+. ./_helpers.bash
+
+fin() {
+  echo "finished" > /tmp/prepare-external-disk.status
+}
+trap fin EXIT
+echo "running" > /tmp/prepare-external-disk.status
+
+EXTERNAL_DISK_PLIST_PATH="${LAUNCH_LOCATION}com.veertu.aws-ec2-mac-amis.prepare-external-disk.plist"
+
+if [[ ! -e "${EXTERNAL_DISK_PLIST_PATH}" ]]; then
+cat > "${EXTERNAL_DISK_PLIST_PATH}" <<EOD
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>Label</key>
+	<string>aws-ec2-mac-amis-prepare-external-disk</string>
+	<key>ProgramArguments</key>
+	<array>
+		<string>/usr/bin/env</string>
+		<string>bash</string>
+		<string>-c</string>
+		<string>/Users/${AWS_INSTANCE_USER:-"ec2-user"}/aws-ec2-mac-amis/prepare-external-disk.bash</string>
+	</array>
+	<key>RunAtLoad</key>
+	<true/>
+	<key>WorkingDirectory</key>
+	<string>/Users/${AWS_INSTANCE_USER:-"ec2-user"}</string>
+	<key>StandardErrorPath</key>
+	<string>/var/log/prepare-external-disk.log</string>
+	<key>StandardOutPath</key>
+	<string>/var/log/prepare-external-disk.log</string>
+</dict>
+</plist>
+EOD
+launchctl load -w "${EXTERNAL_DISK_PLIST_PATH}"
+else
+  # Only run on M4 Macs with external disk
+	if diskutil list /dev/disk4; then
+		if ! diskutil list /dev/disk4 | grep -q disk4s1; then
+			diskutil eraseDisk APFS "Anka" /dev/disk4
+			for u in root ec2-user; do
+				mkdir -p /Volumes/Anka/${u}/img_lib /Volumes/Anka/${u}/state_lib /Volumes/Anka/${u}/vm_lib/.locks
+				chown -R ${u} /Volumes/Anka/${u}
+				if [[ "${u}" == "root" ]]; then
+					anka config img_lib_dir "/Volumes/Anka/${u}/img_lib"
+					anka config state_lib_dir "/Volumes/Anka/${u}/state_lib"
+					anka config vm_lib_dir "/Volumes/Anka/${u}/vm_lib"
+					anka config vm_lock_dir "/Volumes/Anka/${u}/vm_lib/.locks"
+				else
+					sudo -u ${u} anka config img_lib_dir "/Volumes/Anka/${u}/img_lib"
+					sudo -u ${u} anka config state_lib_dir "/Volumes/Anka/${u}/state_lib"
+					sudo -u ${u} anka config vm_lib_dir "/Volumes/Anka/${u}/vm_lib"
+					sudo -u ${u} anka config vm_lock_dir "/Volumes/Anka/${u}/vm_lib/.locks"
+				fi
+			done
+		fi
+	fi
+fi
