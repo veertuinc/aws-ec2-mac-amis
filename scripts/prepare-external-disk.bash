980bed5c79e30eac2b7d9f6aaa61d9eea5dc85ff
Nathan Pierce <norsegaud@gmail.com>
2025-11-27 09:14:48 -0600

    prepare-external for new m4s

--- scripts/prepare-external-disk.bash
+++ scripts/prepare-external-disk.bash
@@ -11,8 +11,10 @@ if diskutil list /dev/disk4; then
 	diskutil list /dev/disk4
 	for username in ec2-user root; do
 		[[ "${username}" == "root" ]] && SUDO="sudo" || SUDO=""
-		${SUDO} mkdir -p /Volumes/Anka/${username}/img_lib /Volumes/Anka/${username}/state_lib /Volumes/Anka/${username}/vm_lib/.locks
-		${SUDO} chown -R ${username} /Volumes/Anka/${username}
+		# Always use sudo for mkdir since volume root is owned by root:wheel
+		sudo mkdir -p /Volumes/Anka/${username}/img_lib /Volumes/Anka/${username}/state_lib /Volumes/Anka/${username}/vm_lib/.locks
+		sudo chown -R ${username} /Volumes/Anka/${username}
+		# Run anka config as the target user
 		${SUDO} anka config img_lib_dir "/Volumes/Anka/${username}/img_lib"
 		${SUDO} anka config state_lib_dir "/Volumes/Anka/${username}/state_lib"
 		${SUDO} anka config vm_lib_dir "/Volumes/Anka/${username}/vm_lib"
