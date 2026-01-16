add45268f9100d0ca2dc9e09036a52f63a0570b6
Nathan Pierce <norsegaud@gmail.com>
2026-01-16 10:36:28 -0600

    quick fixes

--- resize-disk.bash
+++ resize-disk.bash
@@ -2,6 +2,7 @@
 set -exo pipefail
 [[ ! $EUID -eq 0 ]] && echo "RUN AS ROOT!" && exit 1
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo "$(date) Resizing disk..."
 cd $SCRIPT_DIR
 . ./_helpers.bash
 fin() {
